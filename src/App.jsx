36bfec5d63c7ab0d87113c5eaa43125810a22060
Sofia Hechaïchi <sofia.hechaichi@gmail.com>
2024-10-04 19:40:49 +0200

    fix deployment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from './pages/About.jsx';
 import Contact from './pages/Contact.jsx';
 import ProjectTemplate from './templates/projectTemplate.jsx';
 import projectsData from './data/projects-data.js';
-import Navbar from './components/navbar.jsx';
+import Navbar from './components/Navbar.jsx';
 
 const pageStyles = {
 	display: "flex",
